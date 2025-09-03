5bfd18ed1d5ff819d3f1b05ab575ffcfb39d0e2f
Roman Podkorytov <romiraxp@mail.ru>
2025-09-03 18:40:46 +0500

    to check and get an advise

--- src/js/app.js
+++ src/js/app.js
@@ -1,57 +1,23 @@
 // TODO: write code here
 
-// comment this to pass build
-//const unusedVariable = "variable";
-
-// for demonstration purpose only
-//export default function demo(value) {
-//  return `Demo: ${value}`;
-//}
-
-//console.log("app.js included");
 //import { getRandomArbitrary } from "./firstshow";
 //import getRandomArbitrary from "./firstshow";
-document.addEventListener('DOMContentLoaded', () => {
+import goblin_small from "../../pic/goblin_small.png"; // для того, чтобы изображение отображалосьб после сборки webpack, импортируем его
+
+document.addEventListener("DOMContentLoaded", () => {
+  // функция получения случайного значения
   function getRandomValue(min, max) {
-    return Math.round((Math.random() * (max - min) + min),0);
+    return Math.round(Math.random() * (max - min) + min, 0);
   }
-  
-/*  function addIcon(id) {
-    return `
-            <td class="cells" id=${id}><img src="../pic/goblin_small.png"></td>`
-  }*/
 
-/*  function getId() {
-    const randomValue = getRandomValue(0,15);    
-    const _list = document.querySelectorAll('.cells');
-    for (let i = 0; i <= _list.length; i++) {
-      let idValue = _list[i]['id']
-      if (randomValue == idValue) {
-        return idValue;
-      }
-    }
-  }*/
-  
-    //let idValue = getId();  
   const showInterval = setInterval(() => {
-    let randomValue = getRandomValue(0,15);    
-    document.getElementById(randomValue).innerHTML = '<img src="../pic/goblin_small.png">' 
-    setTimeout(() => { 
-      clearInterval(showInterval); 
+    let randomValue = getRandomValue(0, 15); //получаем случайное значение id от 0 до 15, которыми являются ячейки таблицы
+    //document.getElementById(randomValue).innerHTML ='<img src="../pic/goblin_small.png">';
+    document.getElementById(randomValue).innerHTML =
+      `<img src=${goblin_small}>`; // к полученному id тега td приписываем путь к картинке гоблина
+    // остановить процесс показа гоблина случайным порядком по истечении 10 сек
+    setTimeout(() => {
+      clearInterval(showInterval);
     }, 10000);
-  },1000)
-
-/*  const showInterval = setInterval(() => {
-  document.getElementById(getId()).innerHTML = '<img src="../pic/goblin_small.png">' 
-    setTimeout(() => { 
-      clearInterval(showInterval); 
-      //alert('stop'); 
-    }, 60000);
-  },1000)*/
-
-  /*const hideInterval = setInterval(() => {
-  document.getElementById(getId()).innerHTML = 'fgfg' 
-  clearInterval(hideInterval);
-  },2000)*/
-
-})
+  }, 1000);
+});
